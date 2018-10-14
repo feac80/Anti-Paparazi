1fce3760f0a8c4108db0f02cc84f8df5de47465f
Frank <feac8@hotmail.com>
2018-10-14 23:32:57 +0200

    add repository

---
+++ READme.md
@@ -0,0 +1,17 @@
+# Anti-Paparazi
+
+This crhome extension will remove all the photos from a webpage
+
+## Getting Started
+
+1. Clone the repository locally
+2. Navigate to chrome://extensions/
+   2.1 Switch on the developer mode
+   2.2 Click the Load Unpacked button
+   3.Select the folder used in the step 1
+
+## Author
+
+- **Frank Eduardo Araque Cadenas**
+
+## Acknowledgments
--- content.js
+++ content.js
@@ -1,55 +1,9 @@
-function hello() {
-  alert("Hello");
-}
-chrome.runtime.onMessage.addListener(gotMessage);
-
-function gotMessage(request, sender, sendResponse) {
+// Fired when a message is sent from background script (by tabs.sendMessage).
+chrome.runtime.onMessage.addListener(request => {
   if (request == "removeImage") {
-    var removeImages = document.querySelectorAll("img");
-    for (var i = 0; i < removeImages.length; i++) {
-      let imgTag = removeImages[i];
-      // console.log( imgTag.parentNode.parentNode.parentNode);
-
-      if (imgTag.parentNode.parentNode.nodeName == "DIV") {
-        imgTag.parentNode.parentNode.parentNode.removeChild(
-          imgTag.parentNode.parentNode
-        );
-      } else {
-        imgTag.parentNode.parentNode.removeChild(imgTag.parentNode);
-        imgTag.parentNode.removeChild(imgTag);
-      }
-    }
+    const selectedImages = document.querySelectorAll("img");
+    selectedImages.forEach(image => {
+      image.parentNode.removeChild(image);
+    });
   }
-}
-
-// var removeImages = document.querySelectorAll("img");
-
-// for (var i = 0; i < removeImages.length; i++) {
-//   var imgTag = removeImages[i];
-//   if (removeImages[i].parentNode.nodeName == "A") {
-//     console.log("Remove A tag");
-//     // imgTag.previousSibling.parentNode.removeChild(imgTag.previousSibling);
-//     //.removeChild(imgTag.previousSibling);
-//     //imgTag.nextSibling.parentNode.removeChild(imgTag.nextSibling);
-//     //console.log(imgTag.nextSibling);
-
-//     // imgTag.parentNode.parentNode(imgTag.parentNode);
-//     imgTag.parentNode.removeChild(imgTag);
-
-//     //imgTag.parentNode.parentNode.removeChild(imgTag.parentNode);
-//     //console.log(imgTag.parentNode.parentNode);
-//     // imgTag.parentNode.parentNode.parentNode.removeChild(
-//     //  imgTag.parentNode.parentNode
-//     // );
-//     //console.log(imgTag.parentNode.parentNode.parentNode);
-//     //console.log(imgTag.previousSibling.parentNode);
-//   } else {
-//     // imgTag.parentNode.removeChild(imgTag);
-//     console.log("remove img tag");
-//   }
-//   // console.log(removeImages[i].parentNode.nodeName);
-//   // console.log(imgTag.parentElement);
-//   // //console.log(removeImages[i]);
-
-//   // //imgTag.parentNode.removeChild(imgTag);
-// }
+});
